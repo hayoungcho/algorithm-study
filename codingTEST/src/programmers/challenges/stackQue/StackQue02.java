21a889171b8502819d50591beebed35aaff2290b
hayoungCho <jeong06090@naver.com>
2022-04-03 21:49:15 +0900

    temp commit

---
+++ codingTEST/src/programmers/challenges/stackQue/StackQue02.java
@@ -0,0 +1,16 @@
+package programmers.challenges.stackQue;
+
+public class StackQue02 {
+
+	static public int solution(int[] priorities, int location) {
+        int answer = 0;
+        return answer;
+    }
+	
+	public static void maing(String[] args) {
+		int [] proprties = {2, 1, 3, 2};
+		int location = 2;
+		
+		System.out.println(solution(proprties, location));
+	}
+}
