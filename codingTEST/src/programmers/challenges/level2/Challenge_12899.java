b45ae3f2d19ed562140a9ff7ba8aee7289ebb9bb
hayoungCho <jeong06090@naver.com>
2022-05-10 23:15:34 +0900

    edit code - int array to string array

--- codingTEST/src/programmers/challenges/level2/Challenge_12899.java
+++ codingTEST/src/programmers/challenges/level2/Challenge_12899.java
@@ -4,18 +4,15 @@ package programmers.challenges.level2;
 public class Challenge_12899 {
 
 	public static String solution(int n) {
-		int nums[] = new int[]{4, 1, 2}; 
+		//int array일 경우 효율성에서 통과 안 됨.
+//		int nums[] = new int[]{4, 1, 2};
+		
+		String[] nums = {"4", "1", "2"};
         String answer = "";
         
         while(n > 0){
-        	//나머지
-            int remainder = n % 3;
-            n = n / 3;
-            
-            if(remainder == 0)
-            	n--;
-            
-            answer = nums[remainder] + answer;
+            answer = nums[n % 3] + answer;
+            n = (n - 1) / 3;
         }
         
         return answer;
