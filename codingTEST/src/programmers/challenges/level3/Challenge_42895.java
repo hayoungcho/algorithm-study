8a2060efada4a6f6f17ce9fc4fb8e93e3a5b4125
hayoungCho <jeong06090@naver.com>
2022-06-27 17:43:56 +0900

    20220627 firstcommit

---
+++ codingTEST/src/programmers/challenges/level3/Challenge_42895.java
@@ -0,0 +1,17 @@
+package programmers.challenges.level3;
+
+//n으로 표현
+public class Challenge_42895 {
+
+	public static int solution(int N, int number) {
+        int answer = 0;
+        return answer;
+    }
+	
+	public static void main(String args[]) {
+		int N = 5;
+		int number = 12;
+		
+		System.out.println(solution(N, number));
+	}
+}
