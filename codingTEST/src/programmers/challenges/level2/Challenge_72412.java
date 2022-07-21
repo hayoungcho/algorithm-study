77d6c81473e9d0e868f7a806874c205446786e8e
hayoungCho <jeong06090@naver.com>
2022-07-21 16:54:17 +0900

    20220721 - 순위검색 (이분탐색으로 변경) 효율성테스트 통과

--- codingTEST/src/programmers/challenges/level2/Challenge_72412.java
+++ codingTEST/src/programmers/challenges/level2/Challenge_72412.java
@@ -2,88 +2,66 @@ package programmers.challenges.level2;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 //순위 검색
-class Applicant{
-	String language = "";
-	String type = "";
-	String level = "";
-	String food = "";
-	int score = 0;
-	int idx = 0;
-	
-	@Override
-    public boolean equals(Object o) {
-		Applicant compare = (Applicant)o;
-		
-		if(!compare.language.equals("-") && !language.equals(compare.language)) 
-			return false;
-		if(!compare.type.equals("-") && !type.equals(compare.type))
-			return false;
-		if(!compare.level.equals("-") && !level.equals(compare.level)) 
-			return false;
-		if(!compare.food.equals("-") && !food.equals(compare.food))
-			return false;
-		if(score < compare.score) 
-			return false;
-		
-        return true;
-    }
-
-	public Applicant(String language, String type, String level, String food, int score, int idx) {
-		this.language = language;
-		this.type = type;
-		this.level = level;
-		this.food = food;
-		this.score = score;
-		this.idx = idx;
-	}
-}
 
 public class Challenge_72412 {
-
-	 public static int[] solution(String[] info, String[] query) {
-        int[] answer = new int[info.length];
-        List<Applicant> applicantList = new ArrayList<Applicant>();
-        
-        for(int i = 0; i < info.length; i++) {
-        	String[] data = info[i].split(" ");
-        	applicantList.add(new Applicant(data[0], data[1], data[2], data[3], Integer.parseInt(data[4]), i));
-        }
-        Collections.sort(applicantList, (o1, o2) -> o1.score - o2.score);
-
-        List<Applicant> queryList = new ArrayList<Applicant>();
-        for(int i = 0; i < query.length; i++) {
-        	String[] condition = query[i].replace("and", "").replace("  ", " ").split(" ");
-        	queryList.add(new Applicant(condition[0], condition[1], condition[2], condition[3], Integer.parseInt(condition[4]), i));
-        }
-        Collections.sort(queryList, (o1, o2) -> o1.score - o2.score);
-
-//        for(Applicant a : applicantList) 
-//        	System.out.println(a.language + " , " + a.type + " , " + a.level + " , " + a.food + " , " + a.score);
-//        System.out.println("\n\n");
-//        for(Applicant a : queryList) 
-//        	System.out.println(a.language + " , " + a.type + " , " + a.level + " , " + a.food + " , " + a.score);
-//        System.out.println("\n\n");
-
-        
-        int idx = 0;
-    	for(int i = 0; i < queryList.size(); i++) {
-    		Applicant compare = queryList.get(i);
-        	int cnt = 0;
-        	for(int j = idx; j < applicantList.size(); j++) {
-        		if(applicantList.get(j).score >= compare.score) {
-        			if(applicantList.get(j).equals(compare)){
-        				cnt++;
-        			}
-        		}
-        	}
-        	answer[compare.idx] = cnt;
-        	cnt = 0;
-    	}
-        return answer;
-    }
+	static HashMap<String, List<Integer>> map;
+	public static int[] solution(String[] info, String[] query) {
+       int[] answer = new int[query.length];
+       
+       map = new HashMap<String, List<Integer>>();
+       
+       for(int i = 0; i < info.length; i++) {
+    	   String[] strings = info[i].split(" ");
+    	   getAllCaseString(strings, "", 0);
+       }
+       
+       //점수에 대해 오름차순 > 이분탐색을 하기 위함
+       for(String key : map.keySet())
+    	   Collections.sort(map.get(key));
+       
+       for(int i = 0; i < query.length; i++) {
+    	   query[i] = query[i].replaceAll(" and ", "");
+    	   String[] s = query[i].split(" ");
+    	   if(map.containsKey(s[0]))
+    		   answer[i] = search(s[0], Integer.parseInt(s[1]));
+    	   else
+    		   answer[i] = 0;
+       }
+       
+       return answer;
+	}
+	
+	public static int search(String query, int score) {
+		List<Integer> list = map.get(query);
+		int start = 0;
+		int end = list.size() - 1;
+		
+		while(start <= end) {
+			int mid = (start + end)/ 2;
+			if(list.get(mid) < score)
+				start = mid + 1;
+			else
+				end = mid - 1;
+		}
+		return list.size() - start;
+	}
+	
+	public static void getAllCaseString(String[] strings, String key, int cnt) {
+		if(cnt == 4) {
+			if(!map.containsKey(key)) {
+				List<Integer> list = new ArrayList<Integer>();
+				map.put(key, list);
+			}
+			map.get(key).add(Integer.parseInt(strings[4]));
+		}else {
+			getAllCaseString(strings, key + "-", cnt + 1);
+			getAllCaseString(strings, key + strings[cnt], cnt + 1);
+		}
+	}
 	 
 	public static void main(String args[]) {
 		String[] info = {"java backend junior pizza 150","python frontend senior chicken 210","python frontend senior chicken 150","cpp backend senior pizza 260","java backend junior chicken 80","python backend senior chicken 50"};
